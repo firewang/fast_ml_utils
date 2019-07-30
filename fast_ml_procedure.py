0eaa65a3803aa431b4bfbe3b363e5f1bb9b5dc2e
firewang <wanglian2016@gmail.com>
2019-07-30 13:58:13 +0800

    basic framework of ml procedure

---
+++ fast_ml_procedure.py
@@ -0,0 +1,186 @@
+# -*- encoding: utf-8 -*-
+# @Version : 1.0  
+# @Time    : 2019/7/29 10:01
+# @Author  : wanghd
+# @note    : 分类任务全流程搭建，eda 部分抽离作图函数，特征工程部分抽离特征处理函数（类），
+# @note    : 模型调参部分抽离学习器，评估方法，参数字典
+
+import os
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report as cr
+from sklearn.metrics import roc_auc_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+from figure_utils import df_box_plot, df_barplot, df_pair_boxplot, df_pair_plot
+from figure_utils import plot_learning_curve
+from feature_engineer_utils import CustomDummifier, CustomEncoder, my_pipeline
+
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 解决中文显示问题-设置字体为黑体
+plt.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
+sns.set(font="SimHei")
+
+
+def get_data(filename, filepath=os.getcwd()):
+    """读取初始数据"""
+    full_path = os.path.join(filepath, filename)
+    if os.path.exists(full_path):
+        suffix = filename.split(".")[-1]
+        if suffix == 'csv':
+            df = pd.read_csv(full_path)
+        else:
+            df = pd.read_excel(full_path)
+        return df
+    else:
+        raise FileNotFoundError
+
+
+def split_data(df, label_col_name, test_size=0.2):
+    label = df.pop(label_col_name)
+    train_x, test_x, train_y, test_y = train_test_split(df, label, random_state=11, stratify=label, test_size=test_size)
+    return train_x, test_x, train_y, test_y
+
+
+def eda_data(df):
+    """初步的数据探索"""
+    # 先查看数据基本情况
+    df = df.replace("unknown", np.nan)
+
+    # 缺失比例，均值，标准差，分位数等
+    df_info = pd.DataFrame(df.isnull().sum(axis=0), columns=["nan_nums"])
+    df_info.loc[:, 'nan_percent'] = df_info.loc[:, 'nan_nums'] / df.shape[0]
+    df_info.loc[:, 'nunique'] = df.apply(pd.Series.nunique)  # 各列不同值的数量
+    df_info = pd.merge(df_info, df.describe().T, left_index=True, right_index=True, how='left')
+    print(df_info)
+
+    # 初步数据探索
+    # 数值型单变量分布
+    df_box_plot(df.loc[:, ['age', 'balance', 'day']], sub_title='')
+    # 离散型单变量分布
+    df_barplot(df, sub_title='')
+    # 离散型+ 数值型 单变量分布
+    df_pair_boxplot(df, 'housing', ['age', ], 'y', figsize=(12, 16))
+    # 数值型双变量相关性
+    df_pair_plot(df.loc[:, ['age', 'balance', 'day', 'job', 'y']])
+    return None
+
+
+def basic_data_treatment(df):
+    """初步数据处理（预处理）"""
+    # 类别型热编码
+    dummy = CustomDummifier(cols=['marital', 'contact', 'job', "poutcome", 'month'])
+    # df = dummy.fit_transform(df)
+    # 序列型数值编码
+    label_encoder = CustomEncoder(col='education', ordering=['unknown', "primary", "secondary", 'tertiary'])
+    # df = label_encoder.fit_transform(df)
+
+    for col in ["default", "housing", 'loan']:
+        df.loc[:, col] = df.loc[:, col].map({"no": 0, "yes": 1})
+
+    uf_pipeline = my_pipeline(["dum", 'encoder'], [dummy, label_encoder])
+    df = uf_pipeline.fit_transform(df)
+    # pd.set_option('display.max_columns', 30)
+    # print(df)
+    return df
+
+
+def deep_data_treatment(df):
+    """进一步的特征工程：特征选择，特征构造"""
+    return df
+
+
+def basic_model_selection(basic_models=None, x=None, y=None, scoring='roc_auc', cv=5):
+    """初步的算法筛选，确定一个或者几个基学习器进行下一步调参"""
+    if basic_models is None:
+        all_models = [linear_model.LogisticRegression(),
+                      KNeighborsClassifier(),
+                      DecisionTreeClassifier(),
+                      # SVC(),
+                      AdaBoostClassifier(),
+                      AdaBoostClassifier(learning_rate=0.5),
+                      AdaBoostClassifier(base_estimator=linear_model.LogisticRegression()),
+                      RandomForestClassifier()]
+    else:
+        all_models = basic_models
+    cv_scores = []
+    for basic_model in all_models:
+        cv_scores.append(cross_val_score(basic_model, X=x, y=y, scoring=scoring, cv=cv))
+    cv_score_df = pd.DataFrame(cv_scores).T  # 获得各个学习器每折的score
+    cv_score_df.columns = [basic_model.__class__.__name__ for basic_model in all_models]  # 将列名命名为学习器名
+    cv_score_df.index = [f"cv_{cv_round + 1}" for cv_round in cv_score_df.index]  # index命名为cv_{index_round}
+    cv_score_df = pd.concat([cv_score_df, cv_score_df.describe()], axis=0)  # 加入各轮cv的scores的统计信息
+    print(cv_score_df)
+    return cv_score_df
+
+
+def model_param_tuning(train_x, train_y, test_x, test_y, scoring='roc_auc'):
+    """模型调参"""
+    grid_search_param = [{
+        'penalty': ["l1", 'l2'],
+        'C': [1, 10],
+        'solver': ["liblinear"],
+    },
+        # {
+        #     'penalty': ['l2'],
+        #     'C': [0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100],
+        #     'solver': ["lbfgs"],
+        # }
+    ]
+    grid_search_clf = GridSearchCV(linear_model.LogisticRegression(tol=1e-6, max_iter=1000),
+                                   grid_search_param,
+                                   cv=10,
+                                   n_jobs=-1,
+                                   scoring=scoring)
+    grid_search_clf.fit(train_x, train_y)
+    print(grid_search_clf.best_params_)
+    # print(grid_search_clf.best_estimator_.coef_)  # 逻辑回归的各项（特征）系数 == 特征重要性
+    feature_importance = pd.DataFrame(grid_search_clf.best_estimator_.coef_.reshape(-1, 1),
+                                      columns=['Feature_importance'],
+                                      index=train_x.columns)
+    print("{:*^30}".format("特征重要性排序"))
+    print(feature_importance.sort_values(by='Feature_importance', ascending=False))
+    feature_importance.sort_values(by='Feature_importance', ascending=False).plot(kind='barh')
+    plt.show()
+
+    print("{:*^30}".format("最佳模型评估效果"))
+    print(cr(test_y, grid_search_clf.best_estimator_.predict(test_x)))
+
+    print("{:*^30}".format("最佳模型auc值"))
+    print(roc_auc_score(test_y, grid_search_clf.best_estimator_.predict_proba(test_x)[:, 1]))
+    return grid_search_clf.best_estimator_
+
+
+if __name__ == '__main__':
+    data = get_data("train_set.csv")
+    data.pop("ID")
+    test_data = get_data('test_set.csv')
+    test_id = test_data.pop("ID")
+    # print(data.head())
+
+    df = basic_data_treatment(data)
+    test_df = basic_data_treatment(test_data)
+    # eda_data(data)
+
+    # 算法选择
+    train_x, test_x, train_y, test_y = split_data(df, 'y')
+    pd.set_option("display.max_columns", 10)
+    basic_model_selection(basic_models=None, x=train_x, y=train_y, scoring='accuracy', cv=10)
+
+    # 调参
+    best_est = model_param_tuning(train_x, train_y, test_x, test_y)
+
+    pred_label = pd.Series(best_est.predict_proba(test_df)[:, 1], name='pred')
+    pred_df = pd.concat([test_id, pred_label], axis=1)
+    pred_df.to_csv("result.csv", index=False)
+
+    # 判断模型状态
+    plot_learning_curve(best_est, u"学习曲线", train_x, train_y)  # 绘制学习曲线
---
+++ figure_utils.py
@@ -0,0 +1,127 @@
+# -*- encoding: utf-8 -*-
+# @Version : 1.0  
+# @Time    : 2019/7/29 15:12
+# @Author  : wanghd
+# @note    :
+
+from math import sqrt, ceil
+import numpy as np
+from sklearn.model_selection import learning_curve
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 解决中文显示问题-设置字体为黑体
+plt.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
+# sns.set(font="SimHei")
+
+
+def df_box_plot(df, layout=None, sub_title='box_plot'):
+    """数值型变量（单变量）分布情况"""
+    plt.figure(1, figsize=(12, 8))
+    for i, col in enumerate(df.columns):
+        if layout is None:
+            # 如果不指定 layout，则根据列数开方进行计算指定
+            squared_layout = ceil(sqrt(df.shape[1]))
+            plt.subplot(squared_layout, squared_layout, i + 1)
+        else:
+            plt.subplot(*layout, i + 1)
+        plt.boxplot(df.loc[:, col])
+        plt.title(col)
+    plt.suptitle(sub_title)
+    plt.show()
+
+
+def df_barplot(df, layout=None, sub_title="bar_plot"):
+    """离散型变量（单变量）分布情况"""
+    plt.figure(1, figsize=(12, 6))
+    for i, col in enumerate(df.columns):
+        if layout is None:
+            # 如果不指定 layout，则根据列数开方进行计算指定
+            squared_layout = ceil(sqrt(df.shape[1]))
+            plt.subplot(squared_layout, squared_layout, i + 1)
+        else:
+            plt.subplot(*layout, i + 1)
+        count = df[col].value_counts()
+        plt.bar(count.index, count.values, width=0.5)
+        plt.title(col)
+    plt.suptitle(sub_title)
+    plt.show()
+
+
+def df_pair_boxplot(df, x, y, hue, layout=None, figsize=(12, 16), sub_title='pair_boxplot'):
+    """分组箱线图，横轴x(类别型), 纵轴为y（可以为多列，数值型）, 根据hue分组（类别型）
+    即查看数值型变量 在不同类别分组条件下的 分布情况
+    离散型（单变量）和数值型（单变量）"""
+    plt.figure(1, figsize=figsize, dpi=300)
+    for i, col in enumerate(y):
+        if layout is None:
+            # 如果不指定 layout，则根据列数指定行数，列数总为1
+            plt.subplot(len(y), 1, i + 1)
+        else:
+            plt.subplot(*layout, i + 1)
+        sns.boxplot(x, col, hue, df)
+    plt.suptitle(sub_title)
+    plt.show()
+
+
+def df_pair_plot(df):
+    """数值型变量的相关分析图， 会自动过滤非数值型列
+    数值型（双变量）"""
+    sns.pairplot(df)
+    plt.show()
+
+
+def plot_learning_curve(estimator, title, x, y, ylim=None, cv=5, n_jobs=1, train_sizes=np.linspace(.05, 1., 20),
+                        verbose=0, plot=True):
+    """
+    画出data在某模型上的learning curve. 判断当前模型的状态：过拟合，欠拟合
+    https://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html#sphx-glr-auto-examples-model-selection-plot-learning-curve-py
+    ----------
+    estimator : 学习器
+    title : 图像的标题
+    X : 输入的feature，numpy类型
+    y : 输入的target vector
+    ylim : tuple格式的(ymin, ymax), 设定图像中纵坐标的最低点和最高点
+    cv : 做cross-validation的时候，数据分成的份数，其中一份作为cv集，其余n-1份作为training
+    n_jobs : 并行的的任务数(默认1)
+    """
+    train_sizes, train_scores, validation_scores = learning_curve(
+        estimator, x, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, verbose=verbose)
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    validation_scores_mean = np.mean(validation_scores, axis=1)
+    validation_scores_std = np.std(validation_scores, axis=1)
+
+    if plot:
+        plt.figure()
+        plt.title(title)
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.xlabel(u"训练样本数")
+        plt.ylabel(u"得分")
+        plt.gca().invert_yaxis()
+        plt.grid()
+
+        plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std,
+                         alpha=0.1, color="b")
+        plt.fill_between(train_sizes, validation_scores_mean - validation_scores_std,
+                         validation_scores_mean + validation_scores_std,
+                         alpha=0.1, color="r")
+        plt.plot(train_sizes, train_scores_mean, 'o-', color="b", label=u"训练集上得分")
+        plt.plot(train_sizes, validation_scores_mean, 'o-', color="r", label=u"交叉验证集上得分")
+
+        plt.legend(loc="best")
+
+        plt.draw()
+        plt.gca().invert_yaxis()
+        plt.show()
+
+    midpoint = ((train_scores_mean[-1] + train_scores_std[-1]) + (
+            validation_scores_mean[-1] - validation_scores_std[-1])) / 2
+    diff = (train_scores_mean[-1] + train_scores_std[-1]) - (validation_scores_mean[-1] - validation_scores_std[-1])
+    return midpoint, diff
+
+
+if __name__ == '__main__':
+    pass
