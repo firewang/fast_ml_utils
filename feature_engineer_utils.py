51b173a9ecb8ca882d5e5de8d09d0eb0ee475859
firewang <wanglian2016@gmail.com>
2019-08-01 12:19:18 +0800

    增加标准化和归一化快捷方法

--- feature_engineer_utils.py
+++ feature_engineer_utils.py
@@ -39,6 +39,49 @@ class CustomEncoder(TransformerMixin):
         return self
 
 
+def my_scaler(scaler_obj, training_x, testing_x=None, cols=None, **kwargs):
+    """ 标准化和归一化
+     scaler_obj : StandardScaler(),MinMaxScaler(),Normalizer()
+     training_x 训练集df
+     testing_x  测试集df
+     cols 需要转换的列list
+     **kwargs额外参数 : MinMaxScaler() 缩放范围 feature_range ；Normalizer() 正则化方式 norm """
+    scaler_name = scaler_obj.__class__.__name__
+    if scaler_name in ["StandardScaler"]:
+        pass
+    else:
+        if kwargs:
+            params = scaler_obj.get_params()
+            # 更新scaler的参数
+            params.update(kwargs)
+            scaler_obj.set_params(**params)
+
+    if cols is not None:
+        train_x_transformed = pd.DataFrame(scaler_obj.fit_transform(training_x.loc[:, cols]), columns=cols)
+        # 删除原始未转换列
+        training_x.drop(columns=cols, inplace=True)
+        # 构建新的列名index
+        new_cols = list(training_x.columns)
+        new_cols.extend(cols)
+        # 将转换后的列合并回 df
+        training_x = pd.merge(training_x, train_x_transformed, left_index=True, right_index=True)
+        test_x_transformed = pd.DataFrame(scaler_obj.transform(testing_x.loc[:, cols]), columns=cols)
+        # 删除原始未转换列
+        testing_x.drop(columns=cols, inplace=True)
+        # 将转换后的列合并回 df
+        testing_x = pd.merge(testing_x, test_x_transformed, left_index=True, right_index=True)
+    else:
+        # 如果未传入需要缩放的列， 则对所有列进行作用
+        try:
+            training_x = pd.DataFrame(scaler_obj.fit_transform(training_x), columns=training_x.columns)
+            if testing_x is not None:
+                testing_x = pd.DataFrame(scaler_obj.transform(testing_x), columns=testing_x.columns)
+        except ValueError:
+            print("存在非数值的列")
+            raise ValueError
+    return scaler_obj, training_x, testing_x
+
+
 def my_pipeline(names, models):
     my_pipe_line = Pipeline([(name, model) for name, model in zip(names, models)])
     return my_pipe_line
