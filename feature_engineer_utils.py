cfc0247e5299d228eaaf851a41c4dcef944438bb
firewang <wanglian2016@gmail.com>
2019-08-09 14:17:45 +0800

    增加缩减dataframe中数值型列内存消耗方法

--- feature_engineer_utils.py
+++ feature_engineer_utils.py
@@ -5,6 +5,7 @@
 # @note    :
 
 import re
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from sklearn.pipeline import Pipeline
@@ -193,6 +194,39 @@ def my_pipeline(names, models):
     """names: models的别名"""
     my_pipe_line = Pipeline([(name, model) for name, model in zip(names, models)])
     return my_pipe_line
+
+
+def reduce_mem_usage(df, verbose=True):
+    """缩减 df 数值型列的内存消耗体积"""
+    numeric_dtypes = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numeric_dtypes:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose:
+        print(f"Original Mem usage: {start_mem:5.2f} Mb")
+        print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(
+            end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
 
 
 if __name__ == '__main__':
